556454c0b05ec5be9bc93379bb2dcbef358e49a6
Sahil Saini <sahil.saini@nykaa.com>
2020-06-01 16:48:44 +0530

    Service added.

---
+++ SampleFramework/SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Sahil Saini on 6/1/20.
+//  Copyright © 2020 Sahil Saini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
---
+++ SampleFramework/SampleFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  SampleFramework
+//
+//  Created by Sahil Saini on 6/1/20.
+//  Copyright © 2020 Sahil Saini. All rights reserved.
+//
+
+import Foundation
+
+final class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Hello World!"
+    }
+    
+}
